5fae433e2340cafcca83b736a606ad2752ef0167
Ankur Goswami <agoswam3@ucsc.edu>
2017-05-25 11:47:30 -0700

    Add affdexProgram

---
+++ affdexProgram/build/opencv-webcam-demo/CMakeFiles/opencv-webcam-demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leeps/affdexProgram/cpp-sdk-samples/opencv-webcam-demo/opencv-webcam-demo.cpp" "/home/leeps/affdexProgram/build/opencv-webcam-demo/CMakeFiles/opencv-webcam-demo.dir/opencv-webcam-demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/home/leeps/libraries/affdex-sdk/include"
+  "/home/leeps/affdexProgram/cpp-sdk-samples/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ affdexProgram/build/opencv-webcam-demo/CMakeFiles/opencv-webcam-demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv-webcam-demo.dir/opencv-webcam-demo.cpp.o"
+  "opencv-webcam-demo.pdb"
+  "opencv-webcam-demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv-webcam-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
